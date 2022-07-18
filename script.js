bb28ee431d025a2a1bab661952c594f4b72d3da0
abhay <abhay.ban2002@gmail.com>
2022-07-18 21:00:10 +0530

    Correction

--- script.js
+++ script.js
@@ -1,68 +1,47 @@
-class Blog {
-    constructor(title, detail) {
-        this.title = title
-        this.detail = detail
-
+/* Fill your code*/
+class Blog{
+    constructor(title,detail){
+        this.title=title;
+        this.detail=detail;
     }
-    addTitle(innerdiv) {
-
-        let titlecard = document.createElement("h1")
-        titlecard.setAttribute("id", "blog-title")
-        console.log(titlecard)
-        innerdiv.appendChild(titlecard)
-        titlecard.innerHTML = this.title
+    addTitle(){
+        let title_card = document.createElement("h1");
+        title.setAttribute("id","blog-title")
+        console.log(title_card);
+        document.getElementById("card-text").appendChild(title_card);
+        title_card.innerHTML += this.title;
     }
-    addDescription(innerdiv) {
-        let desccard = document.createElement("p")
-        desccard.setAttribute("id", "blog-description")
-        innerdiv.appendChild(desccard)
-        desccard.innerHTML = this.detail
+    addDescription(){
+        let para=document.createElement("p");
+        para.setAttribute("id","blog-description");
+        console.log(para);
+        document.getElementById("card-text").appendChild(para);
+        para.innerHTML += this.detail;
     }
 }
 
+var add = document.getElementById("addBlog");
+add.addEventListener("click",function(){
+    document.getElementById("popupContact").style.display="block";
+});
 
-document.querySelector("#addBlog").addEventListener("click", function() {
-    document.querySelector("#popupContact").style.display = "block"
-    document.querySelector(".body-div2").style.display = "none"
+function popupclose(){
+    document.getElementById("popupContact").style.display="none";   
+}
 
+document.getElementById("close").addEventListener("click", popupclose);
 
+let submitpost = document.getElementById("post");
+submitpost.addEventListener("click", function(){
+    let image = document.createElement("img");
+    image.src="./assets/javascript.png";
+    image.setAttribute("id","image");
+    document.getElementById("card-text").appendChild(image);
 
+    let title = document.getElementById("title").value;
+    let detail = document.getElementById("detail").value;
+    let obj = new Blog(title,detail);
+    obj.addTitle();
+    obj.addDescription();
+    document.getElementById("popupContact").style.display="none";  
 })
-
-document.querySelector("#post").addEventListener("click", function()
-
-    {
-        document.querySelector("#popupContact").style.display = "none"
-        document.querySelector(".body-div2").style.display = "flex"
-
-        let inputtitle = document.querySelector("#title")
-        let inputdescription = document.querySelector("#detail")
-
-        let object = new Blog(inputtitle.value, inputdescription.value)
-
-      
-        let divisontag = document.createElement("div")
-        divisontag.setAttribute("class", "article-card")
-
-        
-        let image = document.createElement("img")
-        let path = './assets/javascript.png'; 
-        image.setAttribute("src", path)
-
-        
-        let innerdiv = document.createElement("div")
-        innerdiv.setAttribute("class", "card-text")
-        innerdiv.setAttribute("id", "card-text")
-         
-        divisontag.appendChild(image)
-        divisontag.appendChild(innerdiv)
-        document.querySelector(".body-div2").appendChild(divisontag)
-
-        console.log(divisontag)
-
-        
-        object.addTitle(innerdiv)
-        object.addDescription(innerdiv)
-
-    }
-)
